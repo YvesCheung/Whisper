a66536d7a66adeeb62ff5b7fa8580a3fc73d1343
zhangyu <975135274@qq.com>
2018-08-15 17:42:48 +0800

    upgrate lint version

--- whisperLint/src/test/java/com/yy/mobile/whisperlint/WhisperHintTest.kt
+++ whisperLint/src/test/java/com/yy/mobile/whisperlint/WhisperHintTest.kt
@@ -1,6 +1,8 @@
 package com.yy.mobile.whisperlint
 
+import com.android.tools.lint.checks.infrastructure.TestFile
 import com.android.tools.lint.checks.infrastructure.TestFiles.java
+import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import org.junit.Test
 
@@ -11,26 +13,67 @@ import org.junit.Test
  */
 class WhisperHintTest {
 
-    private val needErrorAnnotationFile = java("""
-        |package com.yy.mobile.whisper;
-        |import java.lang.annotation.Documented;
-        |import java.lang.annotation.ElementType;
-        |import java.lang.annotation.Inherited;
-        |import java.lang.annotation.Retention;
-        |import java.lang.annotation.RetentionPolicy;
-        |import java.lang.annotation.Target;
-        |
-        |@Documented
-        |@Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE,ElementType.PARAMETER,
-        |   ElementType.CONSTRUCTOR,ElementType.ANNOTATION_TYPE})
-        |@Retention(RetentionPolicy.SOURCE)
-        |@Inherited
-        |public @interface NeedError {
-        |
-        |   String value();
-        |}
-        |
-        """.trimMargin())!!
+    private val needErrorAnnotationFile: TestFile = java("""
+                |package com.yy.mobile.whisper;
+                |
+                |import java.lang.annotation.Documented;
+                |import java.lang.annotation.ElementType;
+                |import java.lang.annotation.Inherited;
+                |import java.lang.annotation.Retention;
+                |import java.lang.annotation.RetentionPolicy;
+                |import java.lang.annotation.Target;
+                |
+                |@Documented
+                |@Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE,ElementType.PARAMETER,
+                |   ElementType.CONSTRUCTOR,ElementType.ANNOTATION_TYPE})
+                |@Retention(RetentionPolicy.SOURCE)
+                |@Inherited
+                |public @interface NeedError {
+                |
+                |   String value();
+                |}
+                |
+                """.trimMargin())
+
+    private val needInfoAnnotationFile: TestFile = java("""
+                |package com.yy.mobile.whisper;
+                |
+                |import java.lang.annotation.Documented;
+                |import java.lang.annotation.ElementType;
+                |import java.lang.annotation.Inherited;
+                |import java.lang.annotation.Retention;
+                |import java.lang.annotation.RetentionPolicy;
+                |import java.lang.annotation.Target;
+                |
+                |@Documented
+                |@Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE,
+                |ElementType.PARAMETER,ElementType.CONSTRUCTOR,ElementType.ANNOTATION_TYPE})
+                |@Retention(RetentionPolicy.SOURCE)
+                |@Inherited
+                |public @interface NeedInfo {
+                |
+                |   String value();
+                |}""".trimMargin())
+
+    private val needWarningAnnotationFile: TestFile = java("""
+                |package com.yy.mobile.whisper;
+                |
+                |import java.lang.annotation.Documented;
+                |import java.lang.annotation.ElementType;
+                |import java.lang.annotation.Inherited;
+                |import java.lang.annotation.Retention;
+                |import java.lang.annotation.RetentionPolicy;
+                |import java.lang.annotation.Target;
+                |
+                |@Documented
+                |@Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE,
+                |ElementType.PARAMETER,ElementType.CONSTRUCTOR,ElementType.ANNOTATION_TYPE})
+                |@Retention(RetentionPolicy.SOURCE)
+                |@Inherited
+                |public @interface NeedWarning {
+                |
+                |   String value();
+                |}""".trimMargin())
 
     @Test
     fun `Check Need Error Method`() {
@@ -63,7 +106,6 @@ class WhisperHintTest {
                 |}
             """.trimMargin()))
             .detector(WhisperHintDetector())
-            .issues(*WhisperHintDetector.getIssue())
             .run()
             .expect("src/com/yy/mobile/TestClass.java:12: Error: i am test [WhisperError]\n" +
                 "       function();\n" +
@@ -74,4 +116,103 @@ class WhisperHintTest {
                 "2 errors, 0 warnings")
     }
 
+    @Test
+    fun `Check Need Warning Method`() {
+
+        lint().files(
+            needWarningAnnotationFile,
+            java("""
+                |package com.yy.mobile;
+                |
+                |public class TestClass {
+                |
+                |   @com.yy.mobile.whisper.NeedWarning("i am test")
+                |   public int function() {
+                |       return 3;
+                |   }
+                |
+                |   TestClass(){
+                |       int a = function();
+                |   }
+                |}
+            """.trimMargin()),
+            kotlin("""
+                |package com.yy.mobile.whisper
+                |
+                |class TestKotlin {
+                |
+                |   private val aCls = com.yy.mobile.TestClass()
+                |
+                |   fun method():Int = aCls.function()
+                |}
+            """.trimMargin()))
+            .detector(WhisperHintDetector())
+            .run()
+            .expect("src/com/yy/mobile/TestClass.java:11: Warning: i am test [WhisperWarning]\n" +
+                "       int a = function();\n" +
+                "               ~~~~~~~~~~\n" +
+                "src/com/yy/mobile/whisper/TestKotlin.kt:7: Warning: i am test [WhisperWarning]\n" +
+                "   fun method():Int = aCls.function()\n" +
+                "                      ~~~~~~~~~~~~~~~\n" +
+                "0 errors, 2 warnings")
+    }
+
+    @Test
+    fun `Check Need Info field`() {
+
+        lint().files(
+            needInfoAnnotationFile,
+            java("""
+                |package com.whisper.mobile;
+                |
+                |import com.yy.mobile.whisper.NeedInfo;
+                |
+                |public class Holo {
+                |
+                |   @NeedInfo("hahaha "+   "asd")
+                |   int field = 3;
+                |
+                |   public Holo(){
+                |       field = 4;
+                |   }
+                |}
+            """.trimMargin()),
+            java("""
+                |package com.whisper.mobile;
+                |
+                |public class HoloChild extends Holo {
+                |
+                |   @com.yy.mobile.whisper.NeedInfo("what the fuck")
+                |   protected Holo parent = new Holo();
+                |
+                |   private void func(){
+                |       int a = field;
+                |   }
+                |}
+            """.trimMargin()),
+            kotlin("""
+                |package com.whisper.another.pkg
+                |
+                |import com.whisper.mobile.HoloChild
+                |
+                |class AnotherHolo : HoloChild() {
+                |
+                |   private fun ini() {
+                |       val a = parent;
+                |   }
+                |}
+            """.trimMargin()))
+            .detector(WhisperHintDetector())
+            .run()
+            .expect("src/com/whisper/another/pkg/AnotherHolo.kt:8: Information: what the fuck [WhisperInfo]\n" +
+                "       val a = parent;\n" +
+                "               ~~~~~~\n" +
+                "src/com/whisper/mobile/Holo.java:11: Information: hahaha asd [WhisperInfo]\n" +
+                "       field = 4;\n" +
+                "       ~~~~~\n" +
+                "src/com/whisper/mobile/HoloChild.java:9: Information: hahaha asd [WhisperInfo]\n" +
+                "       int a = field;\n" +
+                "               ~~~~~\n" +
+                "0 errors, 0 warnings")
+    }
 }
