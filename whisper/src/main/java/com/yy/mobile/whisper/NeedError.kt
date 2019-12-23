fe5fa04f64f11aa5585bb88c9131317886b16a9d
YvesCheung <975135274@qq.com>
2019-12-23 18:33:23 +0800

    add comment

--- whisper/src/main/java/com/yy/mobile/whisper/NeedError.kt
+++ whisper/src/main/java/com/yy/mobile/whisper/NeedError.kt
@@ -1,6 +1,39 @@
 package com.yy.mobile.whisper
 
 /**
+ * [NeedError]可以作用在方法/属性的定义上。添加[NeedError]注解后，IDE会在方法/属性的使用上作出提示。
+ * 比如在一个属性上标记上注解：
+ *
+ * ```Java
+ *  public class A {
+ *
+ *      @NeedError("这个变量有祖传的BUG")
+ *      int field = 3;
+ *  }
+ * ```
+ *
+ * 若在类B中有对注解属性的引用：
+ *
+ * ```Java
+ *  public class B {
+ *
+ *      private void func(){
+ *          int a = field;
+ *      }
+ *  }
+ * ```
+ *
+ * 那么，代码检阅将会在类B上扫描出这段代码，并给予提示：
+ *
+ * ```
+ * src/com/whisper/mobile/B.java:11: Error: 这个变量有祖传的BUG [WhisperError]
+ *      a = field;
+ *        ~~~~~~~~
+ * ```
+ *
+ * @see NeedError
+ * @see NeedWarning
+ *
  * @author YvesCheung
  * 2018/8/13.
  */
