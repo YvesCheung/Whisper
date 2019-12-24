a436272bd37232e0cd2e68f38136aec9f82ff8e8
YvesCheung <975135274@qq.com>
2019-12-24 15:01:21 +0800

    add document

--- whisper/src/main/java/com/yy/mobile/whisper/DeprecatedBy.kt
+++ whisper/src/main/java/com/yy/mobile/whisper/DeprecatedBy.kt
@@ -1,10 +1,48 @@
 package com.yy.mobile.whisper
 
 /**
- * 标记被废弃且可被代替的方法。
- * 可以通过 [.replaceWith] 指定代替的方法名，使用 IDE 的 Show Intention/ Quick Fix 快捷键，
+ * 顾名思义，[DeprecatedBy] 和 [java.lang.Deprecated] 作用完全相同，
+ * 唯一区别是，@DeprecatedBy 支持一键把废弃方法替换为新方法！
+ *
+ * 可以通过 [replaceWith] 指定代替的方法名，使用 IDE 的 Show Intention/ Quick Fix 快捷键，
  * 快速修复，使用新方法替换旧方法。
  *
+ * 通常新增方法，都是在废弃的方法上增删可选的参数，或者调整参数的顺序防止与旧方法的签名一致。
+ * 因此 [DeprecatedBy] 注解具有两种方法替换的特性：
+ *
+ * - 调整原方法的参数
+ *
+ * ```Java
+ * @DeprecatedBy(
+ *     replaceWith = "newMethod(%2$s, %1$s, null)",
+ *     message = "This method is deprecated, use newMethod() instead.",
+ *     level = DeprecatedBy.Level.Error
+ * )
+ * public void oldMethod(int param1, @NonNull String param2) {
+ *     newMethod(param2, param1, null);
+ * }
+ *
+ * public void newMethod(@NonNull String param2, int param1, @Nullable String param3) {
+ *     //Do Something.
+ * }
+ * ```
+ *
+ * `replaceWith = "newMethod(%2$s, %1$s, null)"` 表示用 newMethod 来替换废弃方法，
+ * 而且新方法第一个参数与旧方法的第二个参数相同，新方法第二个参数与旧方法第一个参数相同，新方法第三个参数为null。
+ *
+ * - 更改方法的接收者为其他单例
+ *
+ * ```Java
+ * @DeprecatedBy(replaceWith = "newMethod(%s)", receiver = "deprecate.demo.ClassInstead")
+ * public void oldMethod2(int param) {
+ *     ClassInstead.newMethod(param);
+ * }
+ * ```
+ *
+ * `receiver = "deprecate.demo.ClassInstead"` 表示新方法是单例 `ClassInstead` 的静态方法，
+ * 旧方法 `instance.oldMethod2(0)` 会被一键替换成 `ClassInstead.newMethod(0)`。
+ *
+ * 
  * @author YvesCheung
  * 2018/9/8
  */
